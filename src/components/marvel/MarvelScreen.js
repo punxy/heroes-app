97cfbf56207963e04882292b5ebc448321ffeea8
Sebastián Silva <sebasilvac88@gmail.com>
2022-04-19 01:17:33 -0400

    initial components and routes

---
+++ src/components/marvel/MarvelScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const MarvelScreen = () => {
+  return (
+    <div>
+        <h1>Marvel Screen</h1>
+        <hr/>
+    </div>
+  )
+}
---
+++ src/components/ui/NavBar.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+export const Navbar = () => {
+
+    const handleLogout = () => {
+        //TODO: implement this
+        console.log('logout')
+    }
+
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <Link 
+                className="navbar-brand" 
+                to="/"
+            >
+                Asociaciones
+            </Link>
+
+            <div className="navbar-collapse">
+                <div className="navbar-nav">
+
+                    <NavLink 
+                        activenavlink="active"
+                        className="nav-item nav-link" 
+                        to="/marvel"
+                    >
+                        Marvel
+                    </NavLink>
+
+                    <NavLink 
+                        activenavlink="active"
+                        className="nav-item nav-link" 
+                        to="/dc"
+                    >
+                        DC
+                    </NavLink>
+                </div>
+            </div>
+
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+                <ul className="navbar-nav ml-auto">
+
+                    <span className='nav-item nav-link text-info'>Seba</span>
+
+                    <button 
+                        className="nav-item nav-link btn"
+                        onClick={handleLogout}
+                    >
+                        Logout
+                    </button>
+                </ul>
+            </div>
+        </nav>
+    )
+}
